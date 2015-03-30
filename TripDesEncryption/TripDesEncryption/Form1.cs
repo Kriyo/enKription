2a8e14a7d916c8b588d6f27a11cd4c9343d63d55
Kri <krikenn@gmail.com>
2015-03-30 23:39:10 +0100

    Old college stuff commit
    Triple DES encryption simple assignment.

---
+++ TripDesEncryption/TripDesEncryption/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace TripDesEncryption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        byte[] encrypted;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tDes = new TripleDESCryptoServiceProvider();
+            tDes.Key = md5.ComputeHash(utf8.GetBytes(textBox1.Text));
+            tDes.Mode = CipherMode.ECB;
+            tDes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = tDes.CreateEncryptor();
+            encrypted = trans.TransformFinalBlock(utf8.GetBytes(textBox2.Text), 0, utf8.GetBytes(textBox2.Text).Length);
+            textBox3.Text = BitConverter.ToString(encrypted);
+
+           
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            TripleDESCryptoServiceProvider tDes = new TripleDESCryptoServiceProvider();
+            tDes.Key = md5.ComputeHash(utf8.GetBytes(textBox5.Text));
+            tDes.Mode = CipherMode.ECB;
+            tDes.Padding = PaddingMode.PKCS7;
+            ICryptoTransform trans = tDes.CreateDecryptor();
+            textBox4.Text = utf8.GetString(trans.TransformFinalBlock(encrypted, 0, encrypted.Length));
+        }
+    }
+}
